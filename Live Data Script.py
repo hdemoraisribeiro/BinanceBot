b3fdad911f0bd6c0ea1b448edd665d2aab0e7d0e
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-06-19 02:00:47 +0600

    Live Data Script - Better error catch mechanism

--- Live Data Script.py
+++ Live Data Script.py
@@ -1,4 +1,4 @@
-w# Function for printing to files
+# Function for printing to files
 def print_file(ls, file):
     with open(str(file)+'.txt','w') as file:
         file.write(str(ls)[1:-1].replace(' ','').replace('\'',''))
@@ -36,6 +36,7 @@ def filter_symbol(level, list_of_symbols, threshold, time):
 
 # Import Libraries & Load Environment Variables
 import time
+import requests
 import pandas as pd
 import sqlalchemy, os
 from binance import BinanceSocketManager
@@ -61,17 +62,25 @@ def start():
     level1 = [item for item in [item['symbol'] for item in client.get_ticker() if float(item['priceChangePercent'])>10.0] if ('USDT' in item) & ('DOWN' not in item)]
     print_file(level1,'pairs_level_1')
     
-    # print("Level 2 - 6hrs window")
     if(len(level1)>0):
+        
+        # print("Level 2 - 6hrs window")
         level2 = filter_symbol(2, level1, 6, timedelta(hours=12)) #filter_symbol (level, list_of_symbols, threshold, timedelta)
-        # print("\nLevel 3 - 1hrs window")
+        
         if(len(level2)>0):
+            # print("\nLevel 3 - 1hrs window") 
             level3 = filter_symbol(3, level2, 3, timedelta(hours=7))
-            # print('\nLevel 4 - 30min window')
+            
             if(len(level3)>0):
+                # print('\nLevel 4 - 30min window')
                 level4 = filter_symbol(4, level3, 1, timedelta(minutes=390))
                 # print('\n Finish iteration\n')
 
 while(True):
     time.sleep(5)
-    start()
+    try:
+        start()
+    except requests.exceptions.ReadTimeout:
+        print("Timeout occured")
+    except requests.exceptions.ConnectionError:
+        print("ConnectionError occured")
